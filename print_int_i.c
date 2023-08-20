a07e6e512f250db378e14f989ace600a13b40fb1
Naod Taddese <naodtds@gmail.com>
2023-08-20 11:13:44 -0700

    fix in printing

--- print_int_i.c
+++ print_int_i.c
@@ -9,43 +9,28 @@
 int print_int_i(va_list arguments)
 {
 	int num = va_arg(arguments, int);
-	int digit, len = 0, num_digit = 1, num_man;
+	int len = 0, num_digit = 1, num_man;
 
 	if (num >= 0)
 	{
 		num_man = num;
-		while (num_man / 10 != 0)
-		{
-			num_digit = num_digit * 10;
-			num_man /= 10;
-		}
-		num_man = num;
-		while (num_digit > 0)
-		{
-			digit = num_man  / num_digit;
-			_putchar(digit + 48);
-			len++;
-			num_man = num_man % num_digit;
-			num_digit = num_digit / 10;
-		}
 	}
 	else
 	{
-		len += _putchar('-');
+		_putchar('-');
+		len++;
 		num_man = -num;
-		while (num_man / 10 != 0)
-		{
-			num_digit = num_digit * 10;
-			num_man /= 10;
-		}
-		num_man = -num;
-		while (num_digit > 0)
-		{
-			digit = num_man  / num_digit;
-			len += _putchar(digit + 48);
-			num_man = num_man % num_digit;
-			num_digit = num_digit / 10;
-		}
+	}
+	while (num_man / num_digit >= 10)
+	{
+		num_digit = num_digit * 10;
+	}
+	while(num_digit > 0)
+	{
+		_putchar((num_man / num_digit) + 48);
+		len++;
+		num_man = num_man % num_digit;
+		num_digit /= 10;
 	}
 	return (len);
 }
