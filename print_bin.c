edc5b4315d28d61c7ac99942026ecf021248ba4f
amexowubete <amexowubete@gmail.com>
2023-08-21 05:27:15 -0700

    handle binary

--- print_bin.c
+++ print_bin.c
@@ -30,4 +30,18 @@ int bin_len(unsigned int num)
 
 int print_bin(va_list arguments)
 {
+	unsigned int number = va_arg(arguments, unsigned int);
+	int no_bit = bin_len(number);
+	int binary_bits[no_bit], i;
+
+	for (int i = no_bit - 1; i >= 0; i--)
+	{
+		binary_bits[i] = number % 2;
+		number /=2;
+	}
+	for (int i = 0; i < no_bit; i++)
+	{
+		_putchar(binary_bits[i]+'0');
+	}
+	return (no_bit);
 }
