3cd81f667e73a59c43990b44345276e5f5bebeed
Naod Taddese <naodtds@gmail.com>
2023-08-20 23:57:50 -0700

    delete files

---
+++ print_bin.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_bin - prints binary representation of decimal
+ * @arguments: the number to be converted
+ *
+ */
+
+int print_bin(va_list arguments)
+{
+	int num = va_arg(arguments, unsigned int);
+	int binNum[32];
+}
