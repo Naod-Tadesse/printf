8454885d00a828223ff6b04dd10838b036a8c811
amexowubete <amexowubete@gmail.com>
2023-08-21 05:17:06 -0700

    updates

--- print_bin.c
+++ print_bin.c
@@ -1,8 +1,22 @@
 #include "main.h"
 
+
 /**
- * print_bin - prints the binary represenation of decimal
- * @arguments: receives a number
+ * bin_len - calculates the number of bits needed
+ * @num: the number received
+ * Return: the numbar of bit
+ */
+
+
+
+
+/**
+ * print_bin - prints the binary representation of decimal
+ * @arguments: the number received
  * Return: the number of bits
  *
  */
+
+int print_bin(va_list arguments)
+{
+}
