dba00514dd35bfd62809fd7eba0388e25a2a1afc
amexowubete <amexowubete@gmail.com>
2023-08-21 01:46:32 -0700

    file print int

---
+++ print_int_i.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_int_i - prints integer using %i format specifier 
+ * @arguments: list of numbers to be printed
+ * Return: the number of digits
+ */
+int print_int_i(va_list arguments)
+{
+	int num = va_arg(arguments, int), j;
+	int len = 0, num_digit = 1, num_man;
+	char *min_int = "-2147483648";
+
+	if (num == INT_MIN)
+	{
+		for (j = 0; j < 11; j++)
+		{
+			_putchar(min_int[j]);
+		}
+		len = 11;
+	}
+	else
+	{
+	}
+	return (len);
+}
