2b47cee5b74e8308eb22403c92fe46d2d2b7cccc
Naod Taddese <naodtds@gmail.com>
2023-08-22 05:19:42 -0700

    edge cases

--- print_hex_X.c
+++ print_hex_X.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * hex_len - computes the size of an array
+ * @n: number received
+ * Return: size of array
+ *
+ */
+int hex_len(int n)
+{
+	int len_temp = 1;
+
+	while (n / 16 != 0)
+	{
+		n /= 16;
+		len_temp++;
+	}
+	return (len_temp);
+}
+
+/**
  * print_hex_X - prints decimal representation of a number
  * @arguments: the number received
  * Return: the numof digits
@@ -9,23 +27,19 @@
 int print_hex_X(va_list arguments)
 {
 	unsigned int number = va_arg(arguments, unsigned int);
-	int i = 0, t = 0, j;
-	int *hex;
-	int temp = number, len_temp = 1;
+	int i = 0, t = 0, j, val, *hex, temp = number, len_temp = 1;
 
 	if (number == 0)
 	{
 		_putchar('0');
 		return (1);
 	}
-
-	while (temp / 16 != 0)
+	val = hex_len(temp);
+	hex = malloc(val * sizeof(int));
+	if (hex == NULL)
 	{
-		temp /= 16;
-		len_temp++;
+		return (0);
 	}
-	hex = malloc(len_temp * sizeof(int));
-
 	while (number > 0)
 	{
 		t = number % 16;
@@ -43,7 +57,7 @@ int print_hex_X(va_list arguments)
 
 		number = number / 16;
 	}
-	for (j = len_temp - 1; j >= 0; j--)
+	for (j = val - 1; j >= 0; j--)
 	{
 		_putchar(hex[j]);
 	}
--- print_hex_x.c
+++ print_hex_x.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * hex_le - sets length for array
+ * @n: the number recieved
+ * Return: size of array
+ */
+
+int hex_le(int n)
+{
+	int len_temp = 1;
+
+	while (n / 16 != 0)
+	{
+		n /= 16;
+		len_temp++;
+	}
+	return (len_temp);
+}
+/**
  * print_hex_x - prints decimal representation of a number
  * @arguments: the number received
  * Return: the numof digits
@@ -9,27 +26,22 @@
 int print_hex_x(va_list arguments)
 {
 	unsigned int number = va_arg(arguments, unsigned int);
-	int i = 0, t = 0, j;
-	int *hex;
-	int temp = number, len_temp = 1;
+	int i = 0, t = 0, val, j, *hex, temp = number, len_temp = 1;
 
 	if (number == 0)
 	{
 		_putchar('0');
 		return (1);
 	}
-
-	while (temp / 16 != 0)
+	val = hex_le(temp);
+	hex = malloc(val * sizeof(int));
+	if (hex == NULL)
 	{
-		temp /= 16;
-		len_temp++;
+		return (0);
 	}
-	hex = malloc(len_temp * sizeof(int));
-
 	while (number > 0)
 	{
 		t = number % 16;
-
 		if (t < 10)
 		{
 			hex[i] = t + 48;
@@ -40,14 +52,12 @@ int print_hex_x(va_list arguments)
 			hex[i] = t + 87;
 			i++;
 		}
-
 		number = number / 16;
 	}
-	for (j = len_temp - 1; j >= 0; j--)
+	for (j = val - 1; j >= 0; j--)
 	{
 		_putchar(hex[j]);
 	}
-
 	free(hex);
 	return (len_temp);
 }
